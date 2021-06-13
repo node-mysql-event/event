bcd2706f2d9f8c546c3fe04f26ce0acb1091fab4
Shristi Dobhal <sdobhal1234@gmail.com>
2021-06-13 12:21:40 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const request = require('request');
@@ -8,21 +8,63 @@ const multer = require("multer");
 const ejs = require('ejs');
 const fs = require("fs");
 
+//admin login
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+// const router = require('./router');
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, './public/uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+let upload = multer({storage: storage});
+// var upload = multer({ dest: './public/uploads/' })
+
+
+
+//mysql database
+const mysql = require("mysql");
+
+var mysqlConnection=mysql.createConnection({
+  host:"eventdb.cexlqevmgh8c.us-east-2.rds.amazonaws.com",
+  user:"root",
+  password:"",
+  database:"client",
+  multipleStatements : true
+});
+
+mysqlConnection.connect((err)=>{
+  if(!err)
+  {
+    console.log("connected");
+  }   else   {
+    console.log("connection failed");
+  }
+});
+
+
+
 
 require('dotenv').config();
 
 const app = express();
 
 
-app.engine(
-  "handlebars",
-  exphbs({
-    extname: "handlebars",
-    defaultLayout: false,
-    layoutsDir: "views/layouts/"
-  })
-);
-app.set('view engine', 'handlebars');
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     extname: "handlebars",
+//     defaultLayout: false,
+//     layoutsDir: "views/layouts/"
+//   })
+// );
+// app.set('view engine', 'handlebars');
 
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -31,19 +73,65 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('contact');
+
+//fetch databasevar
+logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'ejs');
+
+ app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/registration-form', (req, res) => {
+  res.render('registration-form');
 });
 
 
-app.post('/send', (req, res) => {
+
+app.post('/send', upload.array('uploaded_file'), (req, res) => {
+console.log(req.files);
+var img = new Array();
+  if (!req.files) {
+          console.log("No file received");
 
 
-  if(!req.body.captcha){
-          console.log("err");
-          return res.json({"success":false, "msg":"Capctha is not checked"});
+        } else {
 
-      }
+          for (var i = 0; i < req.files.length; i++) {
+            if (req.files[i]!='undefined') {
+              img[i]=req.files[i].filename;
+
+            }else{
+              img[i]='NULL';
+            }
+          }
+          console.log('file received');
+          console.log(req);
+
+          var sql = "INSERT INTO tests (id, name, email, phone, title, date, time, venue, city, state, code, img, img2, img3, img4, img5, description) VALUES (null , '"+req.body.name+"', '"+req.body.email+"', '"+req.body.phone+"', '"+req.body.title+"', '"+req.body.date+"', '"+req.body.time+"', '"+req.body.venue+"', '"+req.body.city+"', '"+req.body.state+"', '"+req.body.code+"', '" + img[0] + "', '" + img[1] + "', '" + img[2] + "', '" + img[3] + "', '" + img[4] + "', '"+req.body.description+"')";
+
+            mysqlConnection.query(sql, function(err) {
+              if (err) throw err;
+                console.log("record inserted");
+            })
+
+          message = "Successfully! uploaded";
+
+        }
+
+  //google recaptcha
+  // if(!req.body.captcha){
+  //         console.log("err");
+  //         return res.json({"success":false, "msg":"Capctha is not checked"});
+  //
+  //     }
 
 
 
@@ -60,7 +148,8 @@ app.post('/send', (req, res) => {
           //   return res.json({ success: false, msg: 'Failed captcha verification' });
 
           // If successful
-          return res.json({ success: true, msg: 'Captcha passed' });
+          // return res.json({ success: true, msg: 'Captcha passed' });
+          res.render('registration-form');
 
 
         });
@@ -70,61 +159,242 @@ app.post('/send', (req, res) => {
 
 
 
-  const output = `
-    <p>You'r response has been successfully submitted</p>
-    <h3>Personal Details</h3>
-    <ul>
-      <li>Name: ${req.body.name}</li>
-      <li>Email: ${req.body.email}</li>
-      <li>Phone: ${req.body.phone}</li>
-    </ul>
-    <h3>Event Details</h3>
-    <ul>
-      <li>Title: ${req.body.title}</li>
-      <li>Date: ${req.body.date}</li>
-      <li>Time: ${req.body.time}</li>
-      <li>Venue: ${req.body.venue}</li>
-      <li>City: ${req.body.city}</li>
-      <li>State: ${req.body.state}</li>
-      <li>Code: ${req.body.code}</li>
-      <li>Image Uploaded: ${req.body.img}</li>
-      <li>Description: <p>${req.body.description}</p></li>
-    </ul>
 
-  `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-ignoreTLS: false,
-secure: false, // true for 465, false for other ports like 587
-    auth: {
-        user: process.env.USER, // generated ethereal user
-        pass: process.env.PASSWORD  // generated ethereal password
-    }
 
-  });
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: process.env.EMAIL, // sender address
-      to: req.body.email, // list of receivers
-      subject: 'Event Registration', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
-  };
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
+///mail
+    var sql='SELECT * FROM tests';
+    mysqlConnection.query(sql, function (err, data, fields) {
+    if (err) throw err;
+
+
+    const output = `
+      <p>You'r response has been successfully submitted</p>
+      <h3>Personal Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+      </ul>
+      <h3>Event Details</h3>
+      <ul>
+        <li>Title: ${req.body.title}</li>
+        <li>Date: ${req.body.date}</li>
+        <li>Time: ${req.body.time}</li>
+        <li>Venue: ${req.body.venue}</li>
+        <li>City: ${req.body.city}</li>
+        <li>State: ${req.body.state}</li>
+        <li>Code: ${req.body.code}</li>
+
+        <li>Description: <p>${req.body.description}</p></li>
+      </ul>
+
+
+    `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+    ignoreTLS: false,
+    secure: false, // true for 465, false for other ports like 587
+      auth: {
+          user: process.env.USER, // generated ethereal user
+          pass: process.env.PASSWORD  // generated ethereal password
       }
-      console.log('Message sent: %s', info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-      res.render('contact', {msg:'Email has been sent'});
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: process.env.EMAIL, // sender address
+        to: req.body.email, // list of receivers
+        subject: 'Event Registration', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('registration-form', {msg:'Email has been sent'});
+    });
+
+
+    });
+
+
+
+
+
+
+
+
+
+
   });
+
+// app.post('/stats', upload.single('uploaded_file'), function (req, res) {
+//    // req.file is the name of your file in the form above, here 'uploaded_file'
+//    // req.body will hold the text fields, if there were any
+//    console.log(req.file, req.body)
+// });
+
+
+
+  // load static assets
+  app.use('/static', express.static(path.join(__dirname, 'public')))
+  app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+  app.use(session({
+      secret: uuidv4(), //  '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+      resave: false,
+      saveUninitialized: true
+  }));
+
+  // app.use('/route', router);
+
+  // admin-login route
+  app.get('/admin', (req, res) => {
+
+
+
+      // var sql='SELECT * FROM clients';
+      // mysqlConnection.query(sql, function (err, data, fields) {
+      // if (err) throw err;
+      res.render('admin');
+    // });
+
   });
+
+
+ // app.get('/dashboard',(req, res)=>{
+ //   var sql = "SELECT * FROM tests";
+ //   sqlConnection.query(sql, (err, rows)=>{
+ //     if(err)
+ //     {
+ //       console.log("failed to fetch data");
+ //     }
+ //     res.json(rows);
+ //   })
+ // })
+
+
+
+
+
+
+
+
+
+
+ const  credential = {
+     email : "admin@gmail.com",
+     password : "admin123"
+ }
+
+ // login user
+ app.post('/login', (req, res)=>{
+     if(req.body.email == credential.email && req.body.password == credential.password){
+         req.session.user = req.body.email;
+         res.redirect('/dashboard');
+         //res.end("Login Successful...!");
+     }else{
+         res.end("Invalid Username or Password")
+     }
+ });
+
+ // route for dashboard
+ app.get('/dashboard', function(req, res, next) {
+   if(req.session.user){
+
+     var sql='SELECT * FROM tests';
+     mysqlConnection.query(sql, function (err, data, fields) {
+     if (err) throw err;
+     res.render('user-list', { title: 'User List', userData: data, user : req.session.user});
+   });
+
+
+   }else{
+       res.send("Unauthorize User")
+   }
+
+ });
+
+
+ // route for logout
+ app.get('/logout', (req ,res)=>{
+     req.session.destroy(function(err){
+         if(err){
+             console.log(err);
+             res.send("Error")
+         }else{
+
+             res.render('admin', { title: "Express", logout : "logout Successfully...!"})
+         }
+     })
+ })
+
+
+
+ // app.post('/delete/:id', (req, res) => {
+ //     con.query(`DELETE FROM NodeSQL WHERE id=?;`,id, function (err, result) {
+ //         if (err) throw err;
+ //     });
+ // });
+
+ app.use('/delete/:id', function(req, res, next) {
+  var id= req.params.id;
+    var sql = 'DELETE FROM tests WHERE id = ?';
+    mysqlConnection.query(sql, [id], function (err, data) {
+    if (err) throw err;
+    console.log(data.affectedRows + " record(s) updated");
+  });
+  res.redirect('/dashboard');
+
+});
+
+
+app.use('/add/:id', function(req, res, next) {
+ var id= req.params.id;
+   var sql = "INSERT INTO `clients` SELECT * FROM tests WHERE id = ?";
+   var sql2 = 'DELETE FROM tests WHERE id = ?';
+
+   mysqlConnection.query(sql, [id], function (err, data) {
+   if (err) {
+     throw err;
+   }else{
+     mysqlConnection.query(sql2, [id], function (err, data) {
+     if (err) throw err;
+   });
+   }
+
+ });
+
+ res.redirect('/dashboard');
+
+});
+
+
+
+app.get('/events', (req, res) => {
+
+
+
+    var sql='SELECT * FROM clients';
+    mysqlConnection.query(sql, function (err, data, fields) {
+    if (err) throw err;
+    res.render('events', { title: 'User List', userData: data});
+  });
+
+});
+
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started...'));
---
+++ database.js
@@ -0,0 +1,21 @@
+//mysql database
+const mysql = require("mysql");
+
+var mysqlConnection=mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"client",
+  multipleStatements : true
+});
+
+mysqlConnection.connect((err)=>{
+  if(!err)
+  {
+    console.log("connected");
+  }   else   {
+    console.log("connection failed");
+  }
+});
+
+module.exports = mysqlConnection;
